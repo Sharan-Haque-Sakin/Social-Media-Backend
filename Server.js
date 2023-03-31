2fbd86aa8a28a6f0c567779973455a2f3ef9b4d1
Sakin <sakin@imempire.com>
2023-03-31 14:32:46 +0600

    commit

--- Server.js
+++ Server.js
@@ -1,5 +1,6 @@
 // External Imports
-
+const path = require("path");
+const config = require("./config");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
@@ -17,15 +18,16 @@ const UserIn = require("./Routes/Auth");
 require("dotenv").config();
 
 mongoose.set("strictQuery", true);
-mongoose
-  .connect(process.env.MONGODB)
-  .then(() => {
-    console.log("MongoDb connected");
-  })
-  .catch((err) => console.log(err));
+config();
+// mongoose
+//   .connect(process.env.MONGODB)
+//   .then(() => {
+//     console.log("MongoDb connected");
+//   })
+//   .catch((err) => console.log(err));
 
 // Parsers and Middlewares!
-
+app.use(express.static(path.join(__dirname, "./Client/build")));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json());
@@ -33,11 +35,49 @@ app.use(cors());
 app.use(cookieParser());
 
 app.use("/user", Routes);
+app.get("/getname", async (req, res) => {
+  try {
+    const decoded = jwt.verify(req.cookies.authcookie, process.env.secret);
+    const { userName, userId } = decoded;
+    res.status(200).json({
+      userName: userName,
+      userId: userId,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      errors: error,
+    });
+  }
+});
 app.use("/user/auth", UserIn);
 app.use("/posts/", PostRoutes);
-
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./Client/build/index.html"));
+});
 // Port
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log("Your server is running on port 8000"));
+app.listen(PORT, () => {
+  console.log("App listening on port");
+});
+
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URI);
+//     console.log(`MongoDB Connected: ${conn.connection.host}`);
+//   } catch (error) {
+//     console.log(error);
+//     process.exit(1);
+//   }
+// };
+
+// //Routes go here
+
+// //Connect to the database before listening
+// connectDB().then(() => {
+//   app.listen(PORT, () => {
+//     console.log("listening for requests");
+//   });
+// });
